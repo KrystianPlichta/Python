0b580edb4b6ad8263ffce4458fb64ee69cd7ebc7
KrystianPlichta <polandtorros@gmail.com>
2018-12-10 16:15:01 +0100

    Update Trees.py

--- Exercise2/Trees.py
+++ Exercise2/Trees.py
@@ -18,18 +18,18 @@ conifers = Conifers()
 deciduous = Deciduous()
 
 
-class Pines(Conifers):
+class Pine(Conifers):
     pass
 
 
-pines = Pines()
+pine = Pine()
 
 
-class Oaks(Deciduous):
+class Oak(Deciduous):
     pass
 
 
-oaks = Oaks()
+oak = Oak()
 
 print(trees)
 print(isinstance(trees, Trees))
@@ -47,19 +47,19 @@ print(isinstance(deciduous, object))
 print(isinstance(deciduous, Conifers))
 print(isinstance(conifers, Deciduous))
 
-print(oaks)
-print(isinstance(oaks, Oaks))
-print(isinstance(oaks, Deciduous))
-print(isinstance(oaks, Trees))
-print(isinstance(oaks, Conifers))
-print(isinstance(oaks, Pines))
-print(isinstance(oaks, object))
+print(oak)
+print(isinstance(oak, Oak))
+print(isinstance(oak, Deciduous))
+print(isinstance(oak, Trees))
+print(isinstance(oak, Conifers))
+print(isinstance(oak, Pine))
+print(isinstance(oak, object))
 
-print(pines)
-print(isinstance(pines, Pines))
-print(isinstance(pines, Conifers))
-print(isinstance(pines, Trees))
-print(isinstance(pines, Deciduous))
-print(isinstance(pines, Oaks))
-print(isinstance(pines, object))
+print(pine)
+print(isinstance(pine, Pine))
+print(isinstance(pine, Conifers))
+print(isinstance(pine, Trees))
+print(isinstance(pine, Deciduous))
+print(isinstance(pine, Oak))
+print(isinstance(pine, object))
 
